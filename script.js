424816de8a372b6aff2bb3f953b8b7fb59cf2035
sarahbizal <sarah.bizal@gmail.com>
2026-04-04 16:18:31 -0400

    Update error case in operatorFunction

--- script.js
+++ script.js
@@ -27,7 +27,10 @@ function applyOperatorType(inputOperator, inputDigitOne, inputDigitTwo) {
   };
 
   const operatorFunction = operatorFunctionBySymbol[inputOperator];
+  if (operatorFunction !== operatorFunctionBySymbol) {
+    throw new Error("Invalid operator input");
+  }
   return operatorFunction(inputDigitOne, inputDigitTwo);
 }
 
-console.log(applyOperatorType("+", 4, 3));
+console.log(applyOperatorType("?", 4, 3));
